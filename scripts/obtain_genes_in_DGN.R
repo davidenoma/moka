5f1dfe8091cb78fd688e72be85e000d3a0bef1c3
David Enoma <davidenoma@gmail.com>
2024-03-31 17:21:02 -0600

    include final files

--- scripts/obtain_genes_in_DGN.R
+++ scripts/obtain_genes_in_DGN.R
@@ -36,4 +36,5 @@ pvalue_threshold <- as.numeric(args[3])
 
 genes <- get_gene_from_SKAT(skat_results_path, pvalue_threshold)
 common_genes_dgn <- get_intersect_dgn(genes, disgenet_path)
+cat("P-value threshold",pvalue_threshold)
 cat("Number of common genes in Disgenet:", length(common_genes_dgn), "\n")
