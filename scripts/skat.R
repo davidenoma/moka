96f09f785d9c591560288c172e036c4495049ac0
David Enoma <davidenoma@gmail.com>
2024-07-05 10:04:53 -0600

    Age & Sex

--- scripts/skat.R
+++ scripts/skat.R
@@ -39,6 +39,7 @@ perform_skat_test <- function(gene_name, gene_chromosome, region_start, region_e
     age <- FAM_cov$Age
     sex <- FAM_cov$Sex
     y <- FAM_cov$Phenotype
+    y <- ifelse(y == 1, 0, ifelse(y == 2, 1, y))
     obj <- SKAT_Null_Model(y ~ age + sex, out_type = "D")
     genotype_ssd <- paste0(genotype_prefix, ".ssd")
     SSD.INFO <- Open_SSD(genotype_ssd, paste0(genotype_prefix, ".info"))
