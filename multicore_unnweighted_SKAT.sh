be2c7f7982d1d5b2ef319255a731f90741c44dfb
David Enoma <davidenoma@gmail.com>
2024-09-23 13:30:55 -0600

    Update multicore_unnweighted_SKAT.sh

--- multicore_unnweighted_SKAT.sh
+++ multicore_unnweighted_SKAT.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Check if genotype prefix and base directory are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <genotype_prefix> <base_pipeline_directory>"
+# Check if genotype prefix, base directory, and genotype folder are provided
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo "Usage: $0 <genotype_prefix> <base_pipeline_directory> <genotype_folder>"
   exit 1
 fi
 
-# Set genotype prefix and base directory from command-line arguments
+# Set genotype prefix, base directory, and genotype folder from command-line arguments
 genotype_prefix=$1
 base_dir=$2
+genotype_folder=$3
 
 # Create output directory if it doesn't exist
 output_dir="output_association"
@@ -20,11 +21,11 @@ num_cores=$(nproc)
 # Print out the number of cores
 echo "Using $num_cores cores for parallel processing."
 
-# Run Rscript in parallel for chromosomes 1 to 22
-seq 1 22 | parallel -j $num_cores --verbose Rscript ${base_dir}/scripts/skat_unweighted.R $genotype_prefix ${base_dir}/helper/gene_regions.csv ./ {} $output_dir/
+# Run Rscript in parallel for chromosomes 1 to 22 using the genotype folder as input
+seq 1 22 | parallel -j $num_cores --verbose Rscript ${base_dir}/scripts/skat_unweighted.R $genotype_prefix ${base_dir}/helper/gene_regions.csv $genotype_folder {} $output_dir/
 
 # Confirm that all jobs are complete
-echo "All parallel jobs are complete. Proceeding to merge results." 
+echo "All parallel jobs are complete. Proceeding to merge results."
 
 # Merge all results into a single file
 echo -e "Gene_name\tGene_chromosome\tRegion_start\tRegion_end\tQ_test\tpvalue" > "${genotype_prefix}_assoc_test.tsv"
