9b7f931743dc678cfc7541210d8b0d52547f228f
David Enoma <davidenoma@gmail.com>
2024-09-23 13:26:49 -0600

    Update multicore_unnweighted_SKAT.sh

--- multicore_unnweighted_SKAT.sh
+++ multicore_unnweighted_SKAT.sh
@@ -11,7 +11,7 @@ genotype_prefix=$1
 base_dir=$2
 
 # Create output directory if it doesn't exist
-output_dir="${base_dir}/output_association"
+output_dir="output_association"
 mkdir -p $output_dir
 
 # Get the number of available cores
@@ -23,6 +23,9 @@ echo "Using $num_cores cores for parallel processing."
 # Run Rscript in parallel for chromosomes 1 to 22
 seq 1 22 | parallel -j $num_cores --verbose Rscript ${base_dir}/scripts/skat_unweighted.R $genotype_prefix ${base_dir}/helper/gene_regions.csv ./ {} $output_dir/
 
+# Confirm that all jobs are complete
+echo "All parallel jobs are complete. Proceeding to merge results." 
+
 # Merge all results into a single file
 echo -e "Gene_name\tGene_chromosome\tRegion_start\tRegion_end\tQ_test\tpvalue" > "${genotype_prefix}_assoc_test.tsv"
 for file in "${output_dir}/${genotype_prefix}"*; do
@@ -30,3 +33,5 @@ for file in "${output_dir}/${genotype_prefix}"*; do
     tail -n +2 "$file" >> "${genotype_prefix}_assoc_test.tsv"
   fi
 done
+
+echo "Merging complete."
