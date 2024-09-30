911e44eda3df179ba0fee815c621247b849b4c46
David Enoma <davidenoma@gmail.com>
2024-09-30 09:57:17 -0600

    updateing threads to use parallel

--- scripts/obtain_genes_in_DGN.R
+++ scripts/obtain_genes_in_DGN.R
@@ -1,9 +1,17 @@
-# Check the results in the gene annotation databases
+# Load necessary libraries
+if (!requireNamespace("stats", quietly = TRUE)) {
+  install.packages("stats")
+}
 
-# Obtain the gene names from SKAT results
+# Obtain the gene names from SKAT results with Benjamini-Hochberg correction
 get_gene_from_SKAT <- function(skat_results_path, pvalue_threshold) {
   gene_result <- read.csv(skat_results_path, sep = "\t")
-  gene_result <- subset(gene_result, pvalue < pvalue_threshold)
+
+  # Apply the Benjamini-Hochberg correction
+  gene_result$adjusted_pvalue <- p.adjust(gene_result$pvalue, method = "BH")
+
+  # Subset genes based on the adjusted p-value threshold
+  gene_result <- subset(gene_result, adjusted_pvalue < pvalue_threshold)
   genes <- gene_result$Gene_name
   return(genes)
 }
@@ -42,7 +50,7 @@ skat_results_path <- args[1]
 disgenet_path <- args[2]
 pvalue_threshold <- as.numeric(args[3])
 
-# Obtain significant genes from SKAT results
+# Obtain significant genes from SKAT results with Benjamini-Hochberg correction
 genes <- get_gene_from_SKAT(skat_results_path, pvalue_threshold)
 
 # Get genes from SKAT results that are not present in Disgenet database
@@ -58,17 +66,19 @@ validation_ratio_dgn <- length(common_genes_dgn) / length(genes)
 validation_ratio_diff <- length(diff_genes) / length(genes)
 
 # Prepare output text
-output_text <- paste0("P-value threshold: ", pvalue_threshold, "\n",
+output_text <- paste0("P-value threshold (adjusted): ", pvalue_threshold, "\n",
                       "Number of genes in SKAT results: ", length(genes), "\n",
                       "Number of common genes in Disgenet: ", length(common_genes_dgn), "\n",
                       "Number of significant genes not in Disgenet: ", length(diff_genes), "\n",
                       "Validation ratio as Number of genes in DGN/Number of significant genes: ", validation_ratio_dgn, "\n",
                       "Validation ratio as Number of significant genes not in DGN/Number of significant genes: ", validation_ratio_diff, "\n\n")
 cat(output_text)
+
 # Write output to a text file
 file_name <- basename(skat_results_path)  # Extract file name without extension
 pvalue_threshold_str <- gsub("\\.", "_", as.character(pvalue_threshold))  # Replace dot with underscore for valid filename
 output_file <- paste0("output_plots/output_results_", pvalue_threshold_str, "_", gsub("\\..*$", "", file_name), ".txt")  # Construct output file path
+
 # Open the file for writing
 file_conn <- file(output_file, "w")
 # Write the output text to the file
