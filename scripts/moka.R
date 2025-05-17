f820e17e334e796ffb75394ea6651dcb163b65b8
David Enoma <davidenoma@gmail.com>
2025-05-17 16:23:08 -0600

    Implementation of Heritability-Based Spectral Variance Component Adjustment in MOKA

--- scripts/moka.R
+++ scripts/moka.R
@@ -129,11 +129,11 @@ genotype_matrix <- as.matrix(geno_df[, snp_indices])
     fam <- read.table(paste0(genotype_prefix, ".fam"), header = FALSE)
     Y <- fam$V6
     Y <- scale(Y, center = TRUE, scale = FALSE)
-#         # Step 2: Generate GRM using GCTA
-#     system(paste(
-#       "gcta64 --bfile", prefix_skat,
-#       "--make-grm-bin --out", prefix_skat
-#     ))
+        # Step 2: Generate GRM using GCTA
+    system(paste(
+      "gcta64 --bfile", prefix_skat,
+      "--make-grm-bin --out", prefix_skat
+    ))
 #     # ----- Step 2.5: Generate GCTA-compatible .pheno file from .fam -----
 #   pheno_file <- paste0(prefix_skat, ".pheno")
 #   fam <- read.table(paste0(genotype_prefix, ".fam"), header = FALSE)
